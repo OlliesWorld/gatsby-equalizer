84bfdcd5d09618681eac35de69b9a350d87e0d16
Roni <rtlockwoodwork@gmail.com>
2022-04-10 14:32:50 -0600

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  siteMetadata: {
+    siteUrl: "https://www.yourdomain.tld",
+    title: "gatsby-equalizer",
+  },
+  plugins: [
+    "gatsby-plugin-styled-components",
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images",
+        path: "./src/images/",
+      },
+      __key: "images",
+    },
+  ],
+};
